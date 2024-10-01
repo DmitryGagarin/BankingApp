623727642d74947212dab8946589d998936cc597
Dmitrii Gagarin <gagriu@yandex.ru>
2024-10-01 22:28:12 +0300

    connected user service to eureka

---
+++ user-service/src/main/java/com/bank/user_service/UserServiceApplication.java
@@ -0,0 +1,18 @@
+package com.bank.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+
+// docker build -t bank/user-service-container .
+// docker run -p 8082:8082 bank/user-service-container
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
