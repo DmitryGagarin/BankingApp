f02509307d0d16f470723b080642b8034231011a
Dmitrii Gagarin <gagriu@yandex.ru>
2024-10-02 20:51:36 +0300

    set user, repo, controller, base. started moving to docker

---
+++ user-service/src/main/java/com/bank/user_service/contoller/UserRegistrationController.java
@@ -0,0 +1,23 @@
+package com.bank.user_service.contoller;
+
+import com.bank.user_service.model.User;
+import com.bank.user_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserRegistrationController {
+
+    @Autowired
+    public UserRepository userRepository;
+
+    @PostMapping("/register/{name}/{surname}/{email}/{password}")
+    private User registration(@PathVariable String name,
+                              @PathVariable String surname,
+                              @PathVariable String email,
+                              @PathVariable String password) {
+
+        User user = new User(name, surname, email, password);
+        return userRepository.save(user);
+    }
+}
---
+++ user-service/src/main/java/com/bank/user_service/model/User.java
@@ -0,0 +1,49 @@
+package com.bank.user_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "\"user\"")
+public class User {
+    @Id
+    private UUID id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+
+    public User(String name, String surname, String email, String password) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
